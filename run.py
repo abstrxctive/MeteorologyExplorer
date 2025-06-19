aad72e5d89e984b3f7023dc254bd0d98a3647546
abstrxctive <murisa833@gmail.com>
2025-06-19 18:06:24 +0300

    Update run.py

--- run.py
+++ run.py
@@ -13,7 +13,8 @@ from app.middlewares.antispam import AntiSpamMiddleware
 # Настройка логирования
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+    level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
